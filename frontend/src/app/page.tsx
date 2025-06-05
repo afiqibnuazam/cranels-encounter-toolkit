51a88896f0906929174c31fc2dd478f1f619c2f4
Afiq Azam <afiqibnuazam@gmail.com>
2025-06-05 17:44:15 +0800

    feat: theme update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,25 @@
 import EncounterTable from "@/components/EncounterTable";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-4 gap-4 h-[calc(100vh-84px)]">
       {/* BATTLE LOG */}
       <div className="bg-primary-foreground p-4 rounded-lg">
-        <h1 className="text-lg font-light mb-4">Battle Log</h1>
+        <h1 className="text-2xl font-light mb-4">Battle Log</h1>
         <div className="relative overflow-hidden bg-gray-200">
           
         </div>
       </div>
       {/* ENCOUNTER TABLE */}
       <div className="bg-primary-foreground p-4 rounded-lg col-span-2">
-        <h1 className="text-lg font-light mb-4">Combatants by Initiative</h1>
+        <h1 className="text-2xl font-light mb-4">Combatants by Initiative</h1>
         <EncounterTable />
       </div>
 
       {/* COMBATANT INFO */}
       <div className="bg-primary-foreground p-4 rounded-lg">
         {/* <h1>No Combatant Selected</h1> */}
-        <h1 className="text-lg font-light mb-4">Selected Combatant</h1>
+        <h1 className="text-2xl font-light mb-4">Selected Combatant</h1>
         {/* COLLAPSIBLE */}
         <div className="relative overflow-hidden bg-gray-200">
           {/* Image and Name */}
